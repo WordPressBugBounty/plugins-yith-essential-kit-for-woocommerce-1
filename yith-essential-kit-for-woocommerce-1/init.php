6dab7f3de03d8c0c909bd55221feb35ef5ac6cf7
Sean Murphy <sean@iamseanmurphy.com>
2026-07-17 11:38:48 +0000

    2.53.0

--- yith-essential-kit-for-woocommerce-1/init.php
+++ yith-essential-kit-for-woocommerce-1/init.php
@@ -7,15 +7,15 @@
  * Domain Path: /languages/
  * Author: YITH
  * Author URI: https://yithemes.com/
- * Version: 2.52.0
+ * Version: 2.53.0
  * Requires at least: 6.8
  * Tested up to: 7.0
- * WC requires at least: 10.7
- * WC tested up to: 10.9
+ * WC requires at least: 10.8
+ * WC tested up to: 11.0
  *
  * @author YITH <plugins@yithemes.com>
  * @package YITH Essential Kit for Woocommerce #1
- * @version 2.52.0
+ * @version 2.53.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -47,7 +47,7 @@ if ( ! defined( 'YJP_TEMPLATE_PATH' ) ) {
 }
 
 if ( ! defined( 'YJP_VERSION' ) ) {
-	define( 'YJP_VERSION', '2.52.0' );
+	define( 'YJP_VERSION', '2.53.0' );
 }
 
 if ( ! function_exists( 'yith_plugin_registration_hook' ) ) {
